9b10c701e9d7985c6e7b469a83cf49db4f879cd5
Karsten .D <karstenahouangan@gmail.com>
2023-10-07 19:00:14 +0100

    création du composant pour un restaurant

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div id="app">
-
+    <RestaurantCard/>
   </div>
 </template>
 
 <script>
-  export default{
+import RestaurantCard from './components/RestaurantCard.vue';
+  
+  export default {
     name: "App",
+    components: {
+      RestaurantCard
+    }
   }
 </script>
 
 <style>
-
+  @import url('./style/reset.css');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+  #app{
+    font-family: 'Roboto';
+  }
 </style>
---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="restaurant--card">
+    <div class="restaurant--image">
+
+    </div>
+    <div class="restaurant--information">
+      <div class="top">
+        <p class="name">
+          Subway
+        </p>
+        <p class="note">
+          <span>
+            4.5
+          </span>
+        </p>
+      </div>
+      <p class="time">
+          20-30mn
+        </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+    .restaurant--card{
+        width: 32%;
+        height: 30vh;
+    }
+
+    p{
+      margin: 0;
+    }
+    .restaurant--image{
+      background-image: url('../assets/image/bgRestaurant1.webp');
+      height: 70%;
+      background-size: cover;
+      background-position: center;
+      width: auto;
+    }
+    .restaurant--information{
+      margin: 10px 0px;
+      .top{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .note{
+          background-color: #eee;
+          display: flex;
+          height: 30px;
+          width: 30px;
+          justify-content: center;
+          align-items: center;
+          font-size: 0.8rem;
+          border-radius: 50% ;
+        }
+      }
+    }
+</style>
